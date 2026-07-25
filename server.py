ba92ba3bc37201b86e4e2fdb3f8e5a197d706f2b
Iago Ramos <iagorr123@gmail.com>
2026-07-25 15:08:32 -0300

    Desabilitar cache agressivamente no servidor

--- server.py
+++ server.py
@@ -5,7 +5,9 @@ import sys
 
 class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
-        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
         self.send_header('Access-Control-Allow-Origin', '*')
         super().end_headers()
 
